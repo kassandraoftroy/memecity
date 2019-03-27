d916f284e2efdf2bd1ba76ee5db6267f76095ced
superarius <ari@off-the-grid.io>
2019-03-27 14:21:04 -0400

    Change times

--- memesite/views.py
+++ memesite/views.py
@@ -68,7 +68,7 @@ def audience(request):
 
 def enter_chat(request):
 	now = time.time()
-	if now<1553737800+300:
+	if now<1553737800+800:
 		return render(request, "participate.html", {'text':"Sorry, but you haven't seen the full show yet :/"})
 	user = random.choice(Username.objects.all())
 	user.delete()
